342aa4f54e81151e3c6bed11fc3e398f51777793
Woosung Do <84656108+dws0815@users.noreply.github.com>
2022-09-22 20:06:19 +0900

    Creating web page

---
+++ Code/app.py
@@ -0,0 +1,59 @@
+import pickle
+import streamlit as st
+from tmdbv3api import Movie, TMDb
+
+movie=Movie()
+tmdb=TMDb()
+tmdb.api_key='c61c56dfcafbdc74c1685626cd85c9fa'
+tmdb.language='ko-KR'
+
+def get_recommendations(title):
+    #영화 제목을 통해서 전체 데이터 기준 그 영화의 index 값 얻기
+    idx=movies[movies['title']==title].index[0]
+    #코사인 유사도 매트릭스(cosine_sim)에서 idx에 해당하는 데이터를 (idx,유사도) 형태로 얻기
+    sim_scores=list(enumerate(cosine_sim[idx]))
+     # 코사인 유사도 기준으로 내림차순 정렬
+    sim_scores=sorted(sim_scores, key=lambda x:x[1],reverse=True)
+    #자기 자신을 제외한 10개의 추천 영화
+    sim_scores=sim_scores[1:11]
+     # sim_scores에 있는 첫번째 컬럼(인덱스) 를 가져옴
+    movie_indices=[i[0] for i in sim_scores]
+    # 비슷한 영화 10개를 가져옴
+    images=[]
+    titles=[]
+    for i in movie_indices:
+        id=movies['id'].iloc[i]
+        details=movie.details(id)
+        image_path=details['poster_path']
+        if image_path:
+            image_path="https://image.tmdb.org/t/p/w500" + image_path
+        else:
+            image_path='no_image.jpg'
+        images.append(image_path)
+        titles.append(details['title'])
+    return images,titles
+
+       
+
+
+movies=pickle.load(open('movies.pickle','rb'))
+cosine_sim=pickle.load(open('cosine_sim.pickle','rb'))
+
+st.set_page_config(layout='wide')
+st.header('DOFLIX')
+
+movie_list=movies['title'].values
+title=st.selectbox('Pick your movie',movie_list)
+if st.button('Recommend'):
+    with st.spinner('please wait...'):
+        images, titles=get_recommendations(title)
+
+        idx=0
+        for i in range(0,2):
+            cols=st.columns(5)
+            for col in cols:
+                #이미지 삽입
+                col.image(images[idx])
+                #제목 넣기
+                col.write(titles[idx])
+                idx +=1
